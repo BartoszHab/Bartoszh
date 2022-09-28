50337450ded6e33f96b806c8b3ada34f5761de1a
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-28 09:55:35 +0200

    nazwa

--- Programowanie-1E/First project/First project.cpp
+++ Programowanie-1E/First project/First project.cpp
@@ -6,7 +6,81 @@
 
 int main()
 {
+    /*
+    Instrukcja do wyœwietlania informacji na standardowy strumieñ wyjœcia.
+    Domyœlnie, jesli nie zosta³o to zmienione, jest to konsola w której aplikacja
+    zosta³a uruchomiona.
+    Aby wyswietliæ na konsoli nalezy u¿yæ std::cout.
+    Na konosle wywy³amy za pomoca operatora <<.
+    Po nim musi znajdowaæ siê dana do wys³ania.
+    Instrukcjê koñczymy znakiem œrednik.
+    "Hello World!\n" - jest to sta³a o wartoœci tekstowej.
+    Sta³e tekstowe s¹ ograniczone znakami cudzys³owu.
+    * ' (apostrof) - jesli jest jeden znak
+    * " (cudzys³ów) - jesli jest zero lub wiêcej znaków
+    W sta³ych tekstowych moga byæ wstawiane znaki specjalne. Takie znaki
+    zaczynaj¹ siê od znaku \ (backslash) po którym mo¿e siê znajdowaæ:
+    * jedna litera alfabetu angielskiego
+    * trzy cyfry daj¹ce liczbê ósemkow¹
+    Przyk³adowo:
+    * \n - symbol oznaczaj¹cy now¹ liniê.
+    * \t - symbol oznaczaj¹cy znak tabulatora
+    * \\ - symbol oznaczaj¹cy znak \
+    * \' - symbol oznaczaj¹cy znak '
+    * \" - symbol oznaczaj¹cy znak "
+    * \141 - symbol ósemkowy oznaczajacy zgodnie z tablic¹ kodów ASCII znak m³aej literki a
+    */
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
-    std::cout << "Siemano\n \t\t\t\t\t\t\t\t\t\t\tEOk\t\t\t\t\t\t\t\t\t\t\t\t";
+    std::cout << "Siemano\n" << "On mi drink daje i chce ze mna robic jakies dziwne akcje\n" << "A ile ty masz lat?\n" << 17;
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << +56;
+    std::cout << "\n";
+    std::cout << "57";
+    std::cout << "\n";
+    std::cout << -56.7;
+    std::cout << "\n";
+
+    std::cout << "\n";
+    std::cout << "\n";
+
+    std::cout << "\n";
+
+
+    std::cout << "Podaj liczbe:";
+    int numberFromUser;
+    std::cin >> numberFromUser;
+    std::cout << "Podano:" << numberFromUser << "\n";
+
 }
