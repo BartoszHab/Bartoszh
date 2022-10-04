255b984732e5f3b417c6822398917c7d1ff186da
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-04 09:04:49 +0200

    egg

--- Programowanie-1E/TasksWithVariables/TasksWithVariables.cpp
+++ Programowanie-1E/TasksWithVariables/TasksWithVariables.cpp
@@ -2,8 +2,42 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+
+//funkcja która pobierze dwie liczby oraz wykona podstawowe obliczenia artymetyczne
+
+void task1()
+{//                                suma  ró¿nica   iloczyn   iloraz   reszta       
+    int firstNumber, secondNumber, sum, diffrence, product, quotient, rest;
+    std::cout << "Podaj pierwsza liczbe\n";
+        std::cin >> firstNumber;
+    std::cout << "Podaj druga liczbe:\n";
+    std::cin >> secondNumber;
+
+    sum = firstNumber + secondNumber;
+    diffrence = firstNumber - secondNumber;
+    product = firstNumber * secondNumber;
+    quotient = firstNumber / secondNumber;
+    rest = firstNumber % secondNumber;
+
+    std::cout << "Suma: " << sum << "\n";
+    std::cout << "Roznica: " << diffrence << "\n";
+    std::cout << "Iloczyn: " << product << "\n";
+    std::cout << "Iloraz: " << quotient << "\n";
+    std::cout << "Reszta: " << rest << "\n";
+
+
 }
 
+
+
+
+int main()
+{
+    task1();
+
+
+
+
+
+}
